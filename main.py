0db0df8b38dc90c2d24d2c8379b727aa4907c63d
Michael Ludlam <miludlam@gmail.com>
2024-10-10 20:44:44 -0400

    Adding cell class to draw cells instead of individual lines

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from window import Window
-from point import Point
-from line import Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
 
-    # create test points
-    pointA = Point(0, 0)
-    pointB = Point(100, 0)
-    pointC = Point(100, 100)
-    pointD = Point(0, 100)
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
 
-    # create test lines
-    lineAtoB = Line(pointA, pointB)
-    lineBtoC = Line(pointB, pointC)
-    lineCtoD = Line(pointC, pointD)
-    lineDtoA = Line(pointD, pointA)
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(125, 125, 200, 200)
 
-    # draw lines
-    win.draw_line(lineAtoB, "black")
-    win.draw_line(lineBtoC, "black")
-    win.draw_line(lineCtoD, "black")
-    win.draw_line(lineDtoA, "black")
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(225, 225, 250, 250)
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(300, 300, 500, 500)
 
     win.wait_for_close()
 
